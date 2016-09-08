a2acfe7725968caa022c041fb3b13ae363279dbe
Jose Zamora <jgzamora@terpmail.umd.edu>
2016-09-07 20:45:25 -0400

    sup

--- test.js
+++ test.js
@@ -3,6 +3,6 @@ var expect = require('chai').expect
 
 describe('lodash test', function() {
   it('should use any', function() {
-    expect(_.any([null, 0, 'yes', false], Boolean)).to.be.true;
+    expect(_.some([null, 0, 'yes', false], Boolean)).to.be.true;
   });
 });
